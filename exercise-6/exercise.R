9d6b54fca0190de24ff95cd12e1828fd789575c4
Sarah Liu <swliu79@uw.edu>
2018-04-17 16:26:28 -0700

    Prof went over in class, and I can use as reference!

--- exercise-6/exercise.R
+++ exercise-6/exercise.R
@@ -2,17 +2,20 @@
 
 # Install the `nycflights13` package. Load (`library()`) the package.
 # You'll also need to load `dplyr`
-#install.packages("nycflights13")  # should be done already
+# install.packages("nycflights13")  # should be done already
 library(nycflights13)
 library(dplyr)
 
-# Create a dataframe of the average arrival delays for each _destination_, then 
+# Create a dataframe of the average arrival delays for each _destination_, then
 # use `left_join()` to join on the "airports" dataframe, which has the airport
 # information
 # Which airport had the largest average arrival delay?
-
+largest_arrival_delay <- flights %>%
+  group_by(dest) %>%
+  summarise(avg_delay = mean(arr_delay, na.rm = TRUE)) %>%
+  mutate(faa = dest)
 
 # Create a dataframe of the average arrival delay for each _airline_, then use
 # `left_join()` to join on the "airlines" dataframe
 # Which airline had the smallest average arrival delay?
-
+joined <- left_join(largest_arrival_delay, airports, by = 'faa')
